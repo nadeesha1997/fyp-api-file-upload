8af90df71c406a379950e41781bf3b850f4c22b9
nadeesha1997 <nadeeshamadhushan9@gmail.com>
2022-04-13 16:33:19 +0530

    upload succeeded

--- upload/models.py
+++ upload/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
 class Upload(models.Model):
-    timestamp=models.CharField(max_length=20)
-    message=models.CharField(max_length=500)
+    timestamp = models.CharField(max_length=20)
+    message = models.CharField(max_length=500)
+
 
 class FileUpload(models.Model):
-    timestamp=models.CharField(max_length=20)
-    upload_file=models.FileField()
+    timestamp = models.CharField(max_length=20)
+    upload_file = models.FileField(upload_to='db')
+
 
 def __str__(self):
     return f"<Upload data {self.timestamp} message: {self.message}>"
